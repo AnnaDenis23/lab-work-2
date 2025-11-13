cd79bccd24c13d2743d53b792cb47191d5d08cdd
Anastasiya Denisenko <dreminannasta3@gmail.com>
2025-11-13 15:23:44 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace ЛАБА2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AutoScroll = true;
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown2);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown1);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.checkedListBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.chart1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 406);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "До:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 365);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "От:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown2.AutoSize = true;
+            this.numericUpDown2.Location = new System.Drawing.Point(127, 404);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(102, 22);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.AutoSize = true;
+            this.numericUpDown1.Location = new System.Drawing.Point(127, 361);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(102, 22);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Point",
+            "Line",
+            "Spline"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 317);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(15, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Отобразить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Вид графиков:";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(15, 87);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(226, 140);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.Click += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Серии:";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подключить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Hand;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(524, 450);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Денисенко,БИ-22,СГУГиТ,2025";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using ЛАБА2._1;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace ЛАБА2._1
+{
+    public partial class Form1 : Form
+    {
+
+        private List<double> values = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 2;
+          
+
+
+            numericUpDown1.Minimum = -1000;
+            numericUpDown1.Maximum = 1000;
+            numericUpDown2.Minimum = -1000;
+            numericUpDown2.Maximum = 1000;
+            numericUpDown1.Value = 0;
+            numericUpDown2.Value = 100;
+
+
+            button2.Enabled = false;
+
+            numericUpDown1.Validating += NumericUpDown_Validating;
+            numericUpDown2.Validating += NumericUpDown_Validating;
+        }
+
+
+        private void NumericUpDown_Validating(object sender, CancelEventArgs e)
+        {
+            if (values == null || values.Count == 0) return;
+
+            double min = (double)numericUpDown1.Value;
+            double max = (double)numericUpDown2.Value;
+
+            // Проверяем, есть ли точки в выбранном диапазоне
+            bool hasPointsInRange = values.Any(v => v >= min && v <= max);
+
+            if (!hasPointsInRange)
+            {
+                MessageBox.Show($"В диапазоне от {min} до {max} нет точек данных!\n" +
+                               $"Доступный диапазон: от {values.Min():F2} до {values.Max():F2}",
+                               "Предупреждение",
+                               MessageBoxButtons.OK,
+                               MessageBoxIcon.Warning);
+
+                // Можно сбросить значения к допустимым
+                numericUpDown1.Value = (decimal)values.Min();
+                numericUpDown2.Value = (decimal)values.Max();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog openFileDialog1 = new OpenFileDialog() { Filter = "Текстовые файлы (*.txt)|*.txt" };
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+
+             string filename = openFileDialog1.FileName;
+            ReadFile(filename);
+
+            button2.Enabled = values.Count > 0;
+            if (values.Count > 0)
+            {
+                MessageBox.Show($"Загружено {values.Count} точек.\nИнтервал фильтрации: от {numericUpDown1.Value} до {numericUpDown2.Value}");
+                DrawChart();
+            }
+        }
+
+
+        public void ReadFile(string file)
+        {
+            values.Clear();
+
+            try
+            {
+                var lines = File.ReadAllLines(file);
+                foreach (var line in lines)
+                {
+                    if (double.TryParse(line.Trim(), NumberStyles.Any, CultureInfo.InvariantCulture, out double val))
+                    {
+                        values.Add(val);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка чтения файла:" + ex.Message);
+            }
+        }
+
+        private void DrawChart()
+        {
+            chart1.Series.Clear();
+
+            double min = (double)numericUpDown1.Value;
+            double max = (double)numericUpDown2.Value;
+
+            var filteredPoints = values.Where(v => v >= min && v <= max).ToList();
+
+            Series series = new Series("Координаты");
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: series.ChartType = SeriesChartType.Point; break;
+                case 1: series.ChartType = SeriesChartType.Line; break;
+                case 2: series.ChartType = SeriesChartType.Spline; break;
+                default: series.ChartType = SeriesChartType.Spline; break;
+            }
+            series.Color = Color.Blue;
+            series.BorderWidth = 2;
+            series.MarkerStyle = MarkerStyle.Circle;
+            series.MarkerSize = 6;
+
+            for (int i = 0; i < filteredPoints.Count; i++)
+                series.Points.AddXY(i + 1, filteredPoints[i]);
+
+            chart1.Series.Add(series);
+
+            chart1.Titles.Clear();
+            chart1.Titles.Add($"Отображено {filteredPoints.Count} из {values.Count}");
+
+            chart1.ChartAreas[0].AxisX.Title = "Номер точки";
+            chart1.ChartAreas[0].AxisY.Title = "Значение";
+            chart1.ChartAreas[0].RecalculateAxesScale();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (values.Count == 0) {
+                MessageBox.Show("Нет данных для построения графика.");
+                return;
+
+            }
+
+            DrawChart();
+
+           
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (values.Count > 0)
+            {
+                DrawChart();
+
+            }
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            /*    button1.Location = new Point(splitContainer1.Panel1.Width / 2 - 60,
+                splitContainer1.Panel1.Height / 20 - 8);
+                label1.Location = new Point(splitContainer1.Panel1.Width / 2 - 67,
+                splitContainer1.Panel1.Height / 10 + 9);
+                checkedListBox1.Location = new Point(splitContainer1.Panel1.Width / 2 - 57,
+                 splitContainer1.Panel1.Height / 5 - 8);
+                checkedListBox1.Height = splitContainer1.Panel1.Height / 2;
+                button2.Location = new Point(splitContainer1.Panel1.Width / 2 - 62,
+                 splitContainer1.Panel1.Height - splitContainer1.Panel1.Height / 4 - 6);
+                label2.Location = new Point(splitContainer1.Panel1.Width / 2 - 60,
+                 splitContainer1.Panel1.Height - splitContainer1.Panel1.Height / 5 + 13);
+                comboBox1.Location = new Point(splitContainer1.Panel1.Width / 2 - 60,
+                 splitContainer1.Panel1.Height - splitContainer1.Panel1.Height / 5 + 40);
+
+                label3.Location = new Point(splitContainer1.Panel1.Width / 2 - 80, splitContainer1.Panel1.Height / 2 + 10);
+                numericUpDown1.Location = new Point(splitContainer1.Panel1.Width / 2 - 80, splitContainer1.Panel1.Height / 2 + 40);
+                label4.Location = new Point(splitContainer1.Panel1.Width / 2 + 10, splitContainer1.Panel1.Height / 2 + 10);
+                numericUpDown2.Location = new Point(splitContainer1.Panel1.Width / 2 + 10, splitContainer1.Panel1.Height / 2 + 40);
+
+           */
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+           
+            if (values.Count > 0 && button2.Enabled)
+            {
+                DrawChart();
+            }
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+           
+            if (values.Count > 0 && button2.Enabled)
+            {
+                DrawChart();
+
+            }
+
+        }
+
+       
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (values.Count > 0 && button2.Enabled)
+            {
+                DrawChart();
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
